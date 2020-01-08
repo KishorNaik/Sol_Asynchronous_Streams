0e01d7b1ffd0bad0ef7a13f9836aceb74a31b0ba
appvissta <appvissta@gmail.com>
2020-01-08 16:03:00 +0530

    Code Commit

---
+++ Sol_Asynchronous_Streams/Sol_Asynchronous_Streams/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sol_Asynchronous_Streams
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+
+
+            Task.Run(async () => {
+                await foreach (var i in GnerateNumber())
+                {
+                    Console.WriteLine(i);
+                }
+            }).Wait();
+
+            
+        }
+
+        private async static IAsyncEnumerable<int> GnerateNumber()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+    }
+}
